d0f643ac3501d32ad8f01dcf02875ca573dd6294
Anshu Singh <anshu.singh@broadifitech.com>
2024-02-11 21:40:52 +0530

    changed import for banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Container } from "@mui/material";
 import "./App.css";
-import Banner from "./components/banner";
 import Header from "./components/Header";
 import ProjectSection from "./components/ProjectSection";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 import { BrowserRouter as Router } from "react-router-dom";
+import Banner from "./components/Banner";
 
 function App() {
   return (
